507fc233cf9a2d63be4e67a75b6b4a95bcec2219
KoKoLates <the21515@gmail.com>
2025-11-13 23:45:20 -0500

    pipeline testing

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 
+from core import Env
+from planner import Planner
+from coverage import CoverageOptimizer
+from visualizer import Visualizer
 
 
 if __name__ == "__main__":
-    ...
+    env = Env("example.txt")
+
+    cov_opt = CoverageOptimizer(env.grid(), env.obstacles())
+    targets = cov_opt.process()
+
+    planner = Planner()
+    paths = planner.process(targets)
+
+    viz = Visualizer(env)
+    viz.plot(paths)
