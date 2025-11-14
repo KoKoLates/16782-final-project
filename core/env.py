c4f2ab2120599ae2d1adf4da7d12c70e37f30673
KoKoLates <the21515@gmail.com>
2025-11-14 14:09:37 -0500

    update obstacle checking function

--- core/env.py
+++ core/env.py
@@ -1,12 +1,20 @@
 
 class Env():
     def __init__(self, file_path):
-        ...
+        self.map = []
 
-    def grid(self):
-        ...
+    @property
+    def robot_num():
+        """ return robot number """
+        return 1
 
-    def obstacles(self):
-        ...
+    @property
+    def map(self):
+        """ grid size 
+        @return: tuple of x, y"""
+        return 1, 1
+
     
 
+    def is_obstacle(self, index) -> bool:
+        return self.map[index]
