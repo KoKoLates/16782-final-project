ad12f34d5dc3ec1aa08aeeb3fe53fd9e385083c3
Yatinghsu000627 <997726tina@gmail.com>
2025-11-29 23:55:34 -0500

    update conflict checks to avoid mid-point conflicts

--- planner/base.py
+++ planner/base.py
@@ -20,19 +20,24 @@ class Planner():
     def process(self, goals: List[Tuple[int, int]]) -> List[Path]:
         raise NotImplementedError
 
-    @staticmethod
     def check_vertex_conflict(
+        self,
         neighbor_state: State, 
         constraints_list: Set[State]
     ) -> bool:
+        if neighbor_state.x == self.mid_x and neighbor_state.y == self.mid_y:
+            return False
         return neighbor_state in constraints_list
 
-    @staticmethod
     def check_swap_conflict(
+        self,
         current_state: State, 
         neighbor_state: State, 
         constraints_list: Set[State]
     ) -> bool:
+        if (current_state.x == self.mid_x and current_state.y == self.mid_y) or \
+           (neighbor_state.x == self.mid_x and neighbor_state.y == self.mid_y):
+            return False
         swap_state = State(t=neighbor_state.t, x=current_state.x, y=current_state.y)
         prev_swap_state = State(t=current_state.t, x=neighbor_state.x, y=neighbor_state.y)
         return swap_state in constraints_list and prev_swap_state in constraints_list
@@ -120,10 +125,10 @@ class Planner():
                         continue
          
                 if check_collisions:
-                    if Planner.check_vertex_conflict(neighbor_state, constraints_list):
+                    if self.check_vertex_conflict(neighbor_state, constraints_list):
                         continue
                     
-                    if Planner.check_swap_conflict(current_state, neighbor_state, constraints_list):
+                    if self.check_swap_conflict(current_state, neighbor_state, constraints_list):
                         continue
 
                 if dx != 0 and dy != 0:
